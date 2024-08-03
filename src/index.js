c384e95c7d90b60f37a5fdaaac8d0366cb8ede2b
Siil777 <pavel.ivanov.thk@gmail.com>
2024-08-03 17:06:40 +0300

    feat: function to convert int in string, reverse it and convert back in int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let line = Math.abs(n).toString();
+  let reversed = line.split('').reverse().join('');
+  let positive = parseInt(reversed, 10);
+
+  return positive;
 }
